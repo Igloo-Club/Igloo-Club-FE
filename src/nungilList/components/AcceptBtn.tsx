98e43fe736cf951e9c8dfbd6674eb09ae6a0826d
gkswldnjs78 <jw4319han@naver.com>
2024-05-16 14:18:17 +0900

    fix: 눈길 수락하기 버튼 countdown 삭제

--- src/nungilList/components/AcceptBtn.tsx
+++ src/nungilList/components/AcceptBtn.tsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import instance from '../../common/apis/axiosInstanse';
-import CountDown from '../../main/components/CountDown';
+// import CountDown from '../../main/components/CountDown';
 import { theme } from '../../common/styles/theme';
 
 const AcceptNungilBtn = () => {
@@ -26,10 +26,10 @@ const AcceptNungilBtn = () => {
   return (
     <div css={Container}>
       <button type="button" onClick={ClickAcceptBtn} css={SendBtn}>
-        <span css={countdown}>
+        {/* <span css={countdown}>
           <CountDown />
-        </span>
-        안에 눈길 수락하기
+        </span> */}
+        눈길 수락하기
       </button>
     </div>
   );
@@ -49,9 +49,9 @@ const Container = css`
   border-top: 1px solid #e3e3e3;
 `;
 
-const countdown = css`
-  padding: 0.8rem 0.6rem;
-`;
+// const countdown = css`
+//   padding: 0.8rem 0.6rem;
+// `;
 
 const SendBtn = css`
   display: flex;
